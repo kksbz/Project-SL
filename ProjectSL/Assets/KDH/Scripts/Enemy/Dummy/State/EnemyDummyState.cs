da62aa3c0494b1088232a29b6d245c43b891f320
Kimdaehun0824 <garamei72@gmail.com>
2023-04-11 17:44:46 +0900

    [Daehun] -Feat : Enemy에 상태 추가 -Detail : Enemy가 상태에 따른 동작을 수행하게끔 하기 위해서 상태 추가

--- ProjectSL/Assets/KDH/Scripts/Enemy/Dummy/State/EnemyDummyState.cs
+++ ProjectSL/Assets/KDH/Scripts/Enemy/Dummy/State/EnemyDummyState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class Enemy_Dummy_Idle_State : IState
 {
     EnemyDummy enemy;
@@ -17,6 +19,31 @@ public class Enemy_Dummy_Idle_State : IState
 
     public void Update()
     {
+        if (enemy.Targets.IsValidCollection())
+        {
+            enemy.SetState(new Enemy_Dummy_Patrol_State(enemy));
+        }
+    }
+}
 
+public class Enemy_Dummy_Patrol_State : IState
+{
+    EnemyDummy enemy;
+    public Enemy_Dummy_Patrol_State(EnemyDummy newEnemy)
+    {
+        enemy = newEnemy;
+    }
+
+    public void OnEnter()
+    {
+
+    }
+
+    public void OnExit()
+    {
+    }
+
+    public void Update()
+    {
     }
 }
