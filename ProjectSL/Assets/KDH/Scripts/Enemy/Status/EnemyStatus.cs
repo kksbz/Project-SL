709d085a827c88b39ebdd65d179c04f4ba5defe8
Kimdaehun0824 <garamei72@gmail.com>
2023-04-11 17:43:03 +0900

    [Daehun] -Feat : EnemyStatus 인자 추가 -Detail : Enemy가 플레이어 혹은 장애물을 인식하기 위해서 LayerMask를 Status에 추가

--- ProjectSL/Assets/KDH/Scripts/Enemy/Status/EnemyStatus.cs
+++ ProjectSL/Assets/KDH/Scripts/Enemy/Status/EnemyStatus.cs
@@ -1,7 +1,10 @@
 using System;
+using UnityEngine;
 [Serializable]
 public class EnemyStatus : StatusBase
 {
     public float detectionRange = default;
     public float attackRange = default;
+    public LayerMask targetLayerMask = default;
+    public LayerMask obstacleLayerMask = default;
 }
