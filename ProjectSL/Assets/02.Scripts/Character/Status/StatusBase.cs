ed4be8db9b2f6e4dcf00f0ccca4a6953f378c56d
Kimdaehun0824 <garamei72@gmail.com>
2023-04-11 10:54:37 +0900

    [Daehun] -Feat : 공용 스크립트에 플레이어와 에너미가 상속받을 CharacterBase와 관련된 Directory 및 Scripts 추가 -Detail : CharacterBase와 StatusBase Scripts추가 이를 Player와 Enemy가 상속을 받아서 하위에서 상세 동작을 구현할 예정

---
+++ ProjectSL/Assets/02.Scripts/Character/Status/StatusBase.cs
@@ -0,0 +1,12 @@
+
+public class StatusBase
+{
+    public string name = default;
+    public string info = default;
+    public float maxHp = default;
+    public float currentHp = default;
+    public float maxMoveSpeed = default;
+    public float currentMoveSpeed = default;
+    public float maxAttackDamage = default;
+    public float currentAttackDamage = default;
+}
