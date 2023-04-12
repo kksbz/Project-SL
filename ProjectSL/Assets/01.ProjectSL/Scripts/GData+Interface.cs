1d4a50b92dfe890e067886f71c80566db9a907ac
Kimdaehun0824 <garamei72@gmail.com>
2023-04-12 16:47:33 +0900

    [Daehun] -Feat : IInitialize Interface add -Detail : Init함수에 사용 빈도가 높아 해당 부분을 Interface로 작성

---
+++ ProjectSL/Assets/01.ProjectSL/Scripts/GData+Interface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class GData
+{
+    public interface IInitialize
+    {
+        void Init();
+    }
+}
